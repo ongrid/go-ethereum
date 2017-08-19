546666a723c18968a05b52e52f200ebff7275638
Kirill Varlamov <k.varlamov@brain4net.com>
2017-08-19 16:06:48 +0300

    CalcDifficulty shoul always return 1 for private chain tests

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -287,6 +287,7 @@ func (ethash *Ethash) verifyHeader(chain consensus.ChainReader, header, parent *
 // given the parent block's time and difficulty.
 // TODO (karalabe): Move the chain maker into this package and make this private!
 func CalcDifficulty(config *params.ChainConfig, time uint64, parent *types.Header) *big.Int {
+	return big.NewInt(1)
 	next := new(big.Int).Add(parent.Number, big1)
 	switch {
 	case config.IsMetropolis(next):
